892179ccfc0c532a7b347a0580738a955c1d5cc8
Stephan Yu <Stephan.yu@gmail.com>
2014-10-23 11:38:40 -0700

    added sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   resources :users
 
+  # this URL needs to match the github callback url we set when creating our Github client ID and token
+  get '/auth/github/callback', to: 'sessions#create'
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root 'static_page#index'
