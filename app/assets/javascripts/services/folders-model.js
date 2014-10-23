da15fefd9dcc481f6c0132398618994c127a02f1
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-10-22 19:00:00 -0700

    create service for folders

--- app/assets/javascripts/services/folders-model.js
+++ app/assets/javascripts/services/folders-model.js
@@ -1,8 +1,17 @@
-app.factory('Folder', ['$resource', 
-  function($resource) {
-    return $resource(
+app.factory('Folder', ['$resource', '$location',
+  function($resource, $location) {
+    var Fldr = $resource(
       '/folders/:id',
       {id: '@id'},
       {update: {method: "PATCH"}}
     );
-}])
+  return {
+    createFolder: function(name) {
+      var newFolder = new Fldr({name: name})
+      newFolder.$save(function(data) {
+        console.log(data);
+        $location.path("/folder/" + data.id);
+      });
+    }
+  }
+}]);
