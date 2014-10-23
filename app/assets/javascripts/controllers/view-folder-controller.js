4b6fa1de8919d143cec59882ff50aa6cda7ec374
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-10-23 10:38:02 -0700

    update folder name in angular in progress

--- app/assets/javascripts/controllers/view-folder-controller.js
+++ app/assets/javascripts/controllers/view-folder-controller.js
@@ -1,5 +1,8 @@
-app.controller('ViewFolderController', ['$scope', '$routeParams', 'Folder',
-  function($scope, $routeParams, Folder) {
-    // console.log(Folder.folderResource.get({id: $routeParams["id"]}))
-    $scope.view = Folder.folderResource.get({id: $routeParams["id"]})
+app.controller('ViewFolderController', ['$scope', '$routeParams', 'Folder', 'Doc',
+  function($scope, $routeParams, Folder, Doc) {
+    $scope.folder = Folder.folderResource.get({id: $routeParams["id"]});
+    $scope.updateFolderName = function(dan) {
+      Folder.folderResource.update(dan);
+      // console.log(folder);
+    }
   }])
