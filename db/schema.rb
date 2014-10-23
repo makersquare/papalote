f679fecf74347775202143b632d2940806218f8b
Stephan Yu <Stephan.yu@gmail.com>
2014-10-23 10:21:08 -0700

    db:create and db:migrate of users table for omni auth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141022234958) do
+ActiveRecord::Schema.define(version: 20141023155457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,5 +29,15 @@ ActiveRecord::Schema.define(version: 20141022234958) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "github_uid",   null: false
+    t.string   "name",         null: false
+    t.string   "display_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["github_uid"], name: "index_users_on_github_uid", unique: true, using: :btree
 
 end
