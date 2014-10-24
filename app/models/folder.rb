2a9fa3a99226793fc33e719ad71bcb589d758afb
Daniel Olasky <daniel.olasky@gmail.com>
2014-10-24 11:15:22 -0700

    added subfolders to folder model

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,4 +1,7 @@
 class Folder < ActiveRecord::Base
   validates :name, presence: true
   has_many :docs
+  has_many :subfolders, class_name: "Folder", foreign_key: 'parentfolder_id'
+  belongs_to :parentfolder, class_name: Folder
+
 end
