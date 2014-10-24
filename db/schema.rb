28029ad9f7c4ee5e2e70ebdb8f4e160b725e7ba0
Daniel Olasky <daniel.olasky@gmail.com>
2014-10-24 16:53:04 -0700

    created subfolders and parentfolder foreign key in folder model and tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141022234958) do
+ActiveRecord::Schema.define(version: 20141024232306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20141022234958) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "parentfolder_id"
   end
 
 end
