c45197080a7649346f3f5796bf2b0cd59bc77763
Jeff Louie <jpl7690@yahoo.com>
2014-10-24 09:29:18 -0700

    took out commented lines and unneccessary variable display on chat page

--- app/assets/javascripts/controllers/chat-controller.js
+++ app/assets/javascripts/controllers/chat-controller.js
@@ -5,13 +5,11 @@ app.controller('ChatController', ['$scope', '$timeout', 'socket',
 
     $scope.sendMessage = function(data) {
       socket.emit('chat message', data.message);
-      // $scope.messages.push({message: data.message, username: 'Jeff'});
       $scope.message.message = "";
       return false;
     }
 
     socket.on('chat message', function(msg) {
       $scope.messages.push({message: msg, username: "Jeff 2"});
-      console.log($scope.messages);
     })
 }]);
