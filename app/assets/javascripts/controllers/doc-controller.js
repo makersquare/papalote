6182c55b2b80dc7867b826990396fcd59ad5e26b
Peng Gao <puerile22@gmail.com>
2014-10-25 18:13:04 -0700

    fixed when create a new file in folder view, line number doesn't show

--- app/assets/javascripts/controllers/doc-controller.js
+++ app/assets/javascripts/controllers/doc-controller.js
@@ -1,13 +1,13 @@
 app.controller('DocController', ['$scope','Doc', '$timeout', '$route','DocService',
   function($scope, Doc, $timeout, $route, DocService) {
-  $scope.doc = Doc.get({id: $route.current.params.id}, function(data) {
-    DocService.lineNumber = data.content.split("\n").length;
-    if (DocService.lineNumber > 20) {
-      $scope.rowNumber = DocService.lineNumber;
-    } else {
-      $scope.rowNumber = 20;
-    }
-  });
+  // $scope.doc = Doc.get({id: $route.current.params.id}, function(data) {
+  //   DocService.lineNumber = data.content.split("\n").length;
+  //   if (DocService.lineNumber > 20) {
+  //     $scope.rowNumber = DocService.lineNumber;
+  //   } else {
+  //     $scope.rowNumber = 20;
+  //   }
+  // });
 
   $scope.titleEditDisable = true;
   $scope.saveComplete = false;
