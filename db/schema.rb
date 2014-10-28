ab60d7ac623854831b061da4bf07c1ad7c39f910
Jeff Louie <jpl7690@yahoo.com>
2014-10-28 16:45:32 -0700

    chat works with multiple users and displays user's name with their message

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024232306) do
+ActiveRecord::Schema.define(version: 20141028203533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20141024232306) do
     t.integer  "folder_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "chat_log"
   end
 
   create_table "folders", force: true do |t|
