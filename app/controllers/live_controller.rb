7d2c33330f87a01646f09c7baf74168752466a5e
Peng Gao <puerile22@gmail.com>
2014-10-29 12:56:57 -0700

    fixed websocket issues, when a user goes to a different doc page, it will disconnect the old channel first

--- app/controllers/live_controller.rb
+++ app/controllers/live_controller.rb
@@ -1,6 +1,15 @@
 class LiveController < WebsocketRails::BaseController
   def show_all
-    WebsocketRails[:channel_name].trigger(:change_doc, {:content => message[:content]})
+    WebsocketRails[message[:id]].trigger(:change_doc, {:content => message[:content]})
     # broadcast_message :change_doc, {:content => message[:content] }
+  end
+
+  def delete_user
+    WebsocketRails[message].unsubscribe(connection)
+    puts "user disconnected"
+  end
+
+  def user_connected
+    puts 'user connected'
   end
 end
