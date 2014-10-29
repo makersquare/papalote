b7267e88079055608d78df6a68c761a2088ecc2c
Daniel Olasky <daniel.olasky@gmail.com>
2014-10-29 13:43:09 -0700

    added back to parent folder button and functionality to doc view

--- app/assets/javascripts/controllers/doc-controller.js
+++ app/assets/javascripts/controllers/doc-controller.js
@@ -1,11 +1,21 @@
-app.controller('DocController', ['$scope','Doc', '$timeout', '$route','DocService', 'Download',
-  function($scope, Doc, $timeout, $route, DocService, Download) {
+app.controller('DocController', ['$scope','Doc', '$timeout', '$route','DocService', 'Download', '$location',
+  function($scope, Doc, $timeout, $route, DocService, Download, $location) {
 
   $scope.id = $route.current.params.id;
   $scope.doc = Doc.get({id: $route.current.params.id});
+  $scope.doc.$promise.then(function(data){
+    $scope.currentFolderId = data.folder_id
+    if ($scope.currentFolderId === null) {
+      $scope.backToParentView = false;
+    }
+    else {
+      $scope.backToParentView = true;
+    }
+  });
   $scope.titleEditDisable = true;
   $scope.saveComplete = false;
   $scope.downloadDoc = true;
+  $scope.backToParentView = true;
 
   $scope.saveDoc = function(doc) {
     DocService.updateDoc(doc);
@@ -27,4 +37,8 @@ app.controller('DocController', ['$scope','Doc', '$timeout', '$route','DocServic
   };
 
   $scope.createDownloadFile();
+
+  $scope.findParentFolder = function(folder_id){
+    $location.path('/folders/' + folder_id);
+  };
 }]);
