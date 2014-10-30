902d39aa615d668a646eb149328342df81987ed4
Jon Rogozen <jon.rogozen@gmail.com>
2014-10-30 14:33:43 -0700

    change homepage file/doc creation to use object and wrap object in angular controller to send object

--- app/assets/javascripts/controllers/home-page-controller.js
+++ app/assets/javascripts/controllers/home-page-controller.js
@@ -1,9 +1,9 @@
 app.controller('HomePageController', ['$scope', 'Folder',
   function($scope, Folder){
-    $scope.createDoc = function(file_name){
-      Folder.createDocForFolder({folder_id: null, name: $scope.name});
+    $scope.createDoc = function(upload){
+      Folder.createDocForFolder({doc: {folder:null, name: upload.name}});
     };
-    $scope.createFolder = function(folder_name){
-      Folder.createFolder(folder_name)
+    $scope.createFolder = function(upload){
+      Folder.createFolder({folder: {name: upload.name}})
     };
 }]);
