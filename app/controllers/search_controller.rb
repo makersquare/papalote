75eab2516b5fdd0117a58af4bb935c71da8bfdb8
Stephan Yu <Stephan.yu@gmail.com>
2014-10-30 11:41:20 -0700

    changed SQL query in search controller.rb to search for wildcard keyword

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,12 @@
 class SearchController < ApplicationController
   def index
-    @folders = Folder.where(name: params[:keywords]) 
+    wildcard_keyword = "%#{keywords}%"
+
+    @folders = Folder.where("name ILIKE :search", search: wildcard_keyword) 
     @docs = Doc.where(name: params[:keywords])
   
     render json: {folders: @folders, docs: @docs}
   end
 end
+
+# ILIKE for the name - postgres case insensitive version of LIKE
