29905bbbfe35e024f8949bdb71da5b3824393c89
Peng Gao <puerile22@gmail.com>
2014-10-30 18:45:50 -0700

    add drop down for mode, set default value of theme in the drop down menu, set default mode for files with extension as .js .rb .java .html .css

--- app/controllers/live_controller.rb
+++ app/controllers/live_controller.rb
@@ -1,5 +1,4 @@
 class LiveController < WebsocketRails::BaseController
-
   # Sends updates to all subscribers to the same channel (set by document id)
   def channel_broadcast
     WebsocketRails[message[:id]].trigger(:change_doc, {:content => message[:content]})
