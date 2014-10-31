f7832b5f959327105aa61b6e5331fe8d8e9f9d0b
Jeff Louie <jpl7690@yahoo.com>
2014-10-31 10:36:46 -0700

    redid spacing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
-	def show
+  def show
     current_user = User.retrieve_user(session[:user_id])
     session[:user_id] = current_user.id
-		render json: current_user
-	end
+    render json: current_user
+  end
 end
