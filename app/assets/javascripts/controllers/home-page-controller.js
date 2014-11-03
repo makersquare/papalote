5a52838591661b937de50ed8f7f1c82b4ff21c53
Daniel Olasky <daniel.olasky@gmail.com>
2014-11-03 11:19:36 -0800

    refactored user model, homepage controller and nav bar controller to place  in appropriate place, plus add semi colons

--- app/assets/javascripts/controllers/home-page-controller.js
+++ app/assets/javascripts/controllers/home-page-controller.js
@@ -1,13 +1,13 @@
 app.controller('HomePageController', ['$scope', 'Folder', 'User',
   function($scope, Folder, User){
-    $scope.user = User.currentUser.then(function(data){
-      $scope.user = data
-      $scope.guest = data.guest
-    })
+    $scope.user = User.currentUser.$promise.then(function(data){
+      $scope.user = data;
+      $scope.guest = data.guest;
+    });
     $scope.createDoc = function(upload){
       Folder.createDocForFolder({doc: {folder:null, name: upload.name}});
     };
     $scope.createFolder = function(upload){
-      Folder.createFolder({folder: {name: upload.name}})
+      Folder.createFolder({folder: {name: upload.name}});
     };
 }]);
